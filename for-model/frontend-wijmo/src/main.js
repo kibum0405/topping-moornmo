b39e5a1edac00abceed4865d034394601d8e8e3d
SeongWon <59052363+seongwonyang@users.noreply.github.com>
2023-08-08 14:42:20 +0900

    Update main.js

--- for-model/frontend-wijmo/src/main.js
+++ for-model/frontend-wijmo/src/main.js
@@ -147,18 +147,5 @@ new Vue({
 {{/if}}
 
 <function>
-window.$HandleBars.registerHelper('isSelectedSecurity', function (selectedSecurity) {
-    try{
-        var isSelectedSecurity = false
-        for(var i=0; i<selectedSecurity.length; i++){
-            if(selectedSecurity[i] == 'keycloak-security'){
-                isSelectedSecurity =  true;
-            }
-        }
 
-        return isSelectedSecurity;
-    } catch(e){
-        console.log(e)
-    }
-});
 </function>
