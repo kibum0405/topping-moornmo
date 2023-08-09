44c63808ad446e04b63723c1e086f1616e0ffce7
SeongWon <59052363+seongwonyang@users.noreply.github.com>
2023-08-09 16:29:24 +0900

    Create BaseSecurity.vue

---
+++ for-model/frontend-wijmo/src/components/base-ui/BaseSecurity.vue
@@ -0,0 +1,32 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+
+export default {
+    name: 'BaseSecurity',
+    methods: {
+        getUserRole(){
+            if(this.$OAuth){
+                return this.$OAuth.tokenParsed.realm_access.roles.join(",")
+            }else{
+                return ''
+            }
+        },
+        hasRole(roleName){
+            if(!this.$OAuth){
+                return true
+            }
+            return this.getUserRole().includes(roleName);
+        },
+        getUserName(){
+            if(this.$OAuth){
+                return this.$OAuth.idTokenParsed.preferred_username
+            }else{
+                return 'Guest'
+            }
+        },
+    },
+};
+</script>
